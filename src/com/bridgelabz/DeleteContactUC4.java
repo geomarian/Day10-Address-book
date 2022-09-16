216825a798c3d3704bbb387df63cb7d7add6effb
geomarian <geomarian520@gmail.com>
2022-09-16 22:08:13 +0530

    Added: UC4

---
+++ src/com/bridgelabz/DeleteContactUC4.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+/**
+ * @author ASUS
+ *
+ */
+public class DeleteContactUC4 extends AddressBookMainUC1 {
+	
+	public void deleteContact() {
+        System.out.println("confirm the name to delete contact");
+        String confirmName = input.next();
+        
+        for (int i = 0; i < addressBook.size(); i++) {
+            System.out.println(addressBook.get(i));
+            
+			if (AddressBookMainUC1.addressBook.equals(confirmName)) ;
+            AddressBookMainUC1 person = addressBook.get(i);
+            addressBook.remove(person);
+        }
+        System.out.println("deleted");
+    }
+
+}
